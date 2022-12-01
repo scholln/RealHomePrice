f80753a7795c2770360980cb57c83115f4c4d0ea
Russmar14 <marvinru@msu.edu>
2022-12-01 14:53:53 -0500

    Creating description, namespace and placeholder function .R script for documentation with roxygen2

---
+++ Function1.R
@@ -0,0 +1,12 @@
+#' Function purpose like predicting home price with linear model
+#'
+#' @param x A type, must describe what is needed
+#' @param y A number...
+#' @returns A whatever we want to return
+#' @details Background info
+#' @examples
+#' placeholder(1,2)
+placeholder <- function(x,y) {
+  x + y
+}
+

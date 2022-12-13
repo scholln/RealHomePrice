69f7d2257fc0332ac1d761c82733f5f3b121182b
Russmar14 <marvinru@msu.edu>
2022-12-12 22:08:31 -0500

    changes to add data files that can be called

---
+++ R/cleaned_realtordata.R
@@ -0,0 +1,11 @@
+#' Cleaned realtor data
+#'
+#' @description realtor data set loaded to create train/test split for model fits
+#'
+#' @details realtor dataset without NAs
+#' @md
+#' @docType data
+#' @usage data(cleaned_realtordata)
+#' @format 4 columns with price - in dollars, bed, bath, house_size - in sq ft
+#' @export
+"cleaned_realtordata"
